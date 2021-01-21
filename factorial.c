60e48bf9129f222aa87653d13b1494223650428a
Daitsuku <donlawat0@gmail.com>
2021-01-21 15:48:19 +0700

    update factorial

--- factorial.c
+++ factorial.c
@@ -20,19 +20,4 @@ void main(){
 
     printf("Factorial of the num(%d) = %d", f,x);
 
-    // fac(f,f_tmp);
-
 }
-
-// void fac(int f,int f_tmp){
-
-//     int x;
-//     x=1;
-//     while(f_tmp>0){
-//         x = x * f_tmp;
-//         f_tmp--;
-//         printf(x,f_tmp);
-//         sleep(3);
-//     }
-    
-// }
